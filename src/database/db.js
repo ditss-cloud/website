78f2088339b3df27552874e6ec3a8e486657c86a
DitssGanteng <ditss.cloud@gmail.com>
2025-12-06 17:57:28 +0700

    Update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -1,7 +1,4 @@
 import mongoose from 'mongoose';
-
-// PASTIKAN USERNAME BENAR: dftsscloud_db_user (dengan F)
-//const MONGODB_URI = "mongodb+srv://ditsscloud_db_user:qVh3IaSTcvbx8XVu@asuma-cluster.pmod26g.mongodb.net/asuma-api?retryWrites=true&w=majority&appName=Asuma-Cluster";
 const MONGODB_URI = process.env.MONGODB_URI;
 if (!MONGODB_URI) {
   throw new Error('Please define MONGODB_URI environment variable');
