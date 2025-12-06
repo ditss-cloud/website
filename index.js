8c82fe27ea7f2b641a2ab68772499e8f00f8f6f8
DitssGanteng <ditss.cloud@gmail.com>
2025-12-06 19:01:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -199,6 +199,28 @@ app.get("/support", (req, res) => {
   res.sendFile(path.join(__dirname, "page", "support.html"))
 })
 
+app.get("/api/test-logger", (req, res) => {
+  res.json({
+    message: "This should be logged",
+    timestamp: new Date(),
+    note: "Check MongoDB ApiLog collection for this request"
+  });
+});
+
+app.get("/v1/test", (req, res) => {
+  res.json({
+    message: "Versioned API - should be logged",
+    version: "v1"
+  });
+});
+
+app.get("/not-api", (req, res) => {
+  res.json({
+    message: "This should NOT be logged",
+    note: "This endpoint doesn't match API patterns"
+  });
+});
+
 app.use((req, res, next) => {
   const blockedPaths = [
     "/page/",
